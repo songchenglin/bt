95023cfff5ed2f3e6a697833700bd56ed1bec9ad
songchenglin <songchenglin92@gmail.com>
2014-10-22 22:20:28 +0800

    Add main.[ch] file for test

---
+++ btdownload/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include "main.h"
+#include "torrent.h"
+
+
+int main(const int argc, const char **argv)
+{
+    parase_arguments(argc, argv);
+    return 0;
+}
+void print_usage(void)
+{
+    char usage[] = "BT downloader\n"
+        "-f Torrent file name\n"
+        "-p Path to save file(s)\n"
+        "-h help\n";
+    fprintf(stderr, "%s", usage);
+    exit(EXIT_SUCCESS);
+}
+void parase_arguments(const int argc, const char *argv[])
+{
+    int opt;
+    DBG_INFO("%d", argc);
+    if(argc < 1) {
+        print_usage();
+    }
+    while((opt = getopt(argc, argv, "f:p:h")) != -1) {
+        switch(opt) {
+        case 'f':
+            printf("-f %d",(int)strlen(optarg));
+            break;
+        case 'p':
+            break;
+        case 'h':
+        default:
+            print_usage();
+            break;
+        }
+    }
+}
---
+++ btdownload/main.h
@@ -0,0 +1,5 @@
+#ifdef _MAIN_H_
+#define _MAIN_H_
+__attribut__((noreturn)) print_usage(void);
+void parase_arguments(const int argc, const char *argv[]);
+#endif
