5a9b268a722655b42c26f74f99acfffc974254b1
songchenglin <songchenglin92@gmail.com>
2014-10-23 23:16:55 +0800

    Add differnt level debug macro.

--- debug/debug.h
+++ debug/debug.h
@@ -1,6 +1,7 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 #include <stdio.h>
+#include <string.h>
 
 typedef enum {
     DEBUG_LEVEL_INFO = 0,
@@ -17,5 +18,32 @@ void log_print(debug_level_t level, const char *fmt, ...);
     do { \
         log_print(DEBUG_LEVEL_INFO, "[%s:%d](%s)"fmt"\n", \
                 __FILE__, __LINE__, __func__, ##args); \
+    } while(0)
+#define DBG_LOW(fmt, args...) \
+    do { \
+        log_print(DEBUG_LEVEL_LOW, "[%s:%d](%s)"fmt"\n", \
+                __FILE__, __LINE__, __func__, ##args); \
+    } while(0)
+#define DBG_MED(fmt, args...) \
+    do { \
+        log_print(DEBUG_LEVEL_MED, "[%s:%d](%s)"fmt"\n", \
+                __FILE__, __LINE__, __func__, ##args); \
+    } while(0)
+#define DBG_HIGH(fmt, args...) \
+    do { \
+        log_print(DEBUG_LEVEL_HIGH, "[%s:%d](%s)"fmt"\n", \
+                __FILE__, __LINE__, __func__, ##args); \
+    } while(0)
+#define DBG_ERR(fmt, args...) \
+    do { \
+        log_print(DEBUG_LEVEL_ERR, "[%s:%d](%s)"fmt"\n", \
+                __FILE__, __LINE__, __func__, ##args); \
+    } while(0)
+#define DBG_ERRNO(errno, fmt, args...) \
+    do { \
+        log_print(DEBUG_LEVEL_ERR, "[%s:%d](%s)"fmt"\n", \
+                __FILE__, __LINE__, __func__, ##args); \
+        log_print(DEBUG_LEVEL_ERR, "\nReason--->%s\n\n", \
+                strerror(errno)); \
     } while(0)
 #endif
